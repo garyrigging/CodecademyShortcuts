91b5a61edd071829fae5b8431c2a31b369dea831
Gary Timpane <gmtimpane@gmail.com>
2019-09-15 00:43:43 -0400

    removing console logs

--- contentScript.js
+++ contentScript.js
@@ -5,12 +5,10 @@ chrome.runtime.onMessage.addListener(function(command)	{
 function findElementsToClickByInnerHTML(commandString) {	//command string must be the innerhtml of button
 	var elements = document.querySelectorAll('[data-btn="true"]'),
 		elementIndex = 0 ;
-	console.log(elements);
 	while(elementIndex < elements.length){
 		var element = elements[elementIndex];
 		if(typeof element.innerHTML === "string"){
 			if(element.innerHTML.toLowerCase().includes(commandString)){
-				console.log(element);
 				element.click();
 			}
 		}
