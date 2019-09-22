f01761153c7cd875ff138de5963ad8c479ab2682
Gary Timpane <15068521+garyrigging@users.noreply.github.com>
2019-09-22 19:58:16 -0400

    Idle Fix

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+chrome.commands.onCommand.addListener(function(command)	{
+		sendMessageToSelectedTab(command);
+	});
+
+/*chrome.runtime.onSuspend.addListener(function() {
+	chrome.runtime.reload();
+});
+*/
+
 function sendMessageToSelectedTab(commandString)	{
 	chrome.tabs.getSelected(function(selectedTab){
 		chrome.tabs.sendMessage(selectedTab.id, {'command' : commandString});
 	});
 }
-
-chrome.commands.onCommand.addListener(function(command)	{
-		sendMessageToSelectedTab(command);
-	});
-
-chrome.runtime.onSuspend.addListener(function() {
-	chrome.runtime.reload();
-});
