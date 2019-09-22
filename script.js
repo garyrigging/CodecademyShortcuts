6833e59f3f1b128709aa5957b3a9e743a70c2f89
Gary Timpane <15068521+garyrigging@users.noreply.github.com>
2019-09-22 19:42:14 -0400

    Idle Fix

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-chrome.commands.onCommand.addListener(function(command)	{
-		sendMessageToSelectedTab(command);
-});
-
 function sendMessageToSelectedTab(commandString)	{
 	chrome.tabs.getSelected(function(selectedTab){
 		chrome.tabs.sendMessage(selectedTab.id, {'command' : commandString});
 	});
 }
+
+chrome.commands.onCommand.addListener(function(command)	{
+		sendMessageToSelectedTab(command);
+	});
+
+chrome.idle.onStateChanged.addListener(function() {
+	chrome.commands.onCommand.addListener(function(command)	{
+		sendMessageToSelectedTab(command);
+	});
+});
