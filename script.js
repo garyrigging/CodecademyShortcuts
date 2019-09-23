85c541bad2f850bd2c282f629c69e1b9b12d04f4
Gary Timpane <15068521+garyrigging@users.noreply.github.com>
2019-09-22 21:15:14 -0400

    Idle Fix

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-chrome.commands.onCommand.addListener(function(command)	{
+var runCount = 0;
+chrome.webNavigation.onCompleted.addListener(function() { 
+	console.log('running ' + runCount);
+	runCount++;
+	chrome.commands.onCommand.addListener(function(command)	{
 		sendMessageToSelectedTab(command);
-});
+	});
+}, {url: [{urlMatches : 'https://www.codecademy.com/*'}]});
 
 function sendMessageToSelectedTab(commandString)	{
 	chrome.tabs.getSelected(function(selectedTab){
