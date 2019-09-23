21551b175fa8f6fdd675e90e4f7da165f2cb69d4
Gary Timpane <15068521+garyrigging@users.noreply.github.com>
2019-09-22 20:02:49 -0400

    Revert

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 chrome.commands.onCommand.addListener(function(command)	{
 		sendMessageToSelectedTab(command);
-	});
-
-/*chrome.runtime.onSuspend.addListener(function() {
-	chrome.runtime.reload();
 });
-*/
 
 function sendMessageToSelectedTab(commandString)	{
 	chrome.tabs.getSelected(function(selectedTab){
